2857cb9c256d87ae49c3ba2272e94e56d1f2878b
小手川智紀 <kotegawatomonori@kotsumaru.local>
2020-06-04 06:01:49 +0900

    Updates

---
+++ precache-manifest.4119eb7449a9a86b3a0473b250d7c07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a881bc251ab17085e22b17bcabe73f3",
+    "url": "/dartslife/index.html"
+  },
+  {
+    "revision": "8490fe20ef16dabcf865",
+    "url": "/dartslife/static/css/main.d733631f.chunk.css"
+  },
+  {
+    "revision": "621b9e4ae821ddc613d3",
+    "url": "/dartslife/static/js/2.98c3dbfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dartslife/static/js/2.98c3dbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8490fe20ef16dabcf865",
+    "url": "/dartslife/static/js/main.e6cef9de.chunk.js"
+  },
+  {
+    "revision": "13776a1b709a49a5e830",
+    "url": "/dartslife/static/js/runtime-main.2609483e.js"
+  },
+  {
+    "revision": "90a9f9207c8f54558e5d956f2b443e55",
+    "url": "/dartslife/static/media/dartslife-back.90a9f920.png"
+  }
+]);
